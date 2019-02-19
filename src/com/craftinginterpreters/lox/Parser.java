cafa7048774952216ea716fda3f0db76db9759a8
septa97 <jancharles.adona@gmail.com>
2019-02-19 10:13:54 +0800

    Add initial parser

---
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -0,0 +1,175 @@
+package com.craftinginterpreters.lox;
+
+import com.craftinginterpreters.expr.Expr;
+import com.craftinginterpreters.expr.subexpr.Binary;
+import com.craftinginterpreters.expr.subexpr.Grouping;
+import com.craftinginterpreters.expr.subexpr.Literal;
+import com.craftinginterpreters.expr.subexpr.Unary;
+
+import java.util.List;
+
+class Parser {
+  private static final class ParseError extends RuntimeException{}
+
+  private final List<Token> tokens;
+  private int current = 0;
+
+  Parser(List<Token> tokens) {
+    this.tokens = tokens;
+  }
+
+  Expr parse() {
+    try {
+      return expression();
+    } catch (ParseError error) {
+      return null;
+    }
+  }
+
+  private Expr expression() {
+    return equality();
+  }
+
+  private Expr equality() {
+    Expr expr = comparison();
+
+    while (match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)) {
+      Token operator = previous();
+      Expr right = comparison();
+      expr = new Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr comparison() {
+    Expr expr = addition();
+
+    while (match(TokenType.GREATER, TokenType.GREATER_EQUAL, TokenType.LESS, TokenType.LESS_EQUAL)) {
+        Token operator = previous();
+        Expr right = addition();
+        expr = new Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr addition() {
+    Expr expr = multiplication();
+
+    while (match(TokenType.PLUS, TokenType.MINUS)) {
+      Token operator = previous();
+      Expr right = multiplication();
+      expr = new Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr multiplication() {
+    Expr expr = unary();
+
+    while (match(TokenType.STAR, TokenType.SLASH)) {
+      Token operator = previous();
+      Expr right = unary();
+      expr = new Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr unary() {
+      if (match(TokenType.BANG, TokenType.MINUS)) {
+        Token operator = previous();
+        Expr right = unary();
+        return new Unary(operator, right);
+      }
+
+      return primary();
+  }
+
+  private Expr primary() {
+      if (match(TokenType.FALSE)) return new Literal(false);
+      if (match(TokenType.TRUE)) return new Literal(true);
+      if (match(TokenType.NIL)) return new Literal(null);
+
+      if (match(TokenType.NUMBER, TokenType.STRING)) {
+        return new Literal(previous().literal);
+      }
+
+      if (match(TokenType.LEFT_PAREN)) {
+        Expr expr = expression();
+        consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
+        return new Grouping(expr);
+      }
+
+      throw error(peek(), "Expect expression.");
+  }
+
+  private Token consume(TokenType type, String message) {
+      if (check(type)) return advance();
+
+      throw error(peek(), message);
+  }
+
+  private ParseError error(Token token, String message) {
+      Lox.error(token, message);
+
+      return new ParseError();
+  }
+
+  private void synchronize() {
+      advance();
+
+      while (!isAtEnd()) {
+        if (previous().type == TokenType.SEMICOLON) return;
+
+        switch (peek().type) {
+          case CLASS:
+          case FUN:
+          case VAR:
+          case FOR:
+          case IF:
+          case WHILE:
+          case PRINT:
+          case RETURN:
+              return;
+        }
+
+        advance();
+      }
+  }
+
+  private boolean match(TokenType... types) {
+    for (TokenType type : types) {
+      if (check(type)) {
+        advance();
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private boolean check(TokenType type) {
+    if (isAtEnd()) return false;
+    return peek().type == type;
+  }
+
+  private Token advance() {
+    if (!isAtEnd()) current++;
+    return previous();
+  }
+
+  private boolean isAtEnd() {
+    return peek().type == TokenType.EOF;
+  }
+
+  private Token peek() {
+    return tokens.get(current);
+  }
+
+  private Token previous() {
+    return tokens.get(current - 1);
+  }
+}
